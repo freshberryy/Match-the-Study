bde4d5cf6080c79090606498147552b2e94db8aa
quokka486 <jmgdp123@naver.com>
2024-08-24 02:25:35 +0900

    #소셜로그인 버튼 디자인 추가 네이버, 카카오톡 로그인 버튼 디자인 추가 구글, 페이스북 로그인 버튼 디자인 제거
    #임시로고 추가/한글화
    ms 임시로고 추가
    템플릿의 기존 텍스트들 한글화

--- src/main/java/com/example/ms/user/UserController.java
+++ src/main/java/com/example/ms/user/UserController.java
@@ -2,9 +2,18 @@ package com.example.ms.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     @Autowired
     private UserService userService;
+
+
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
 }
